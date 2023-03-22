b4b51815c4c98c393c51c0b5ec063d79a761ac50
Дима Дзюбенко <dzubenkod7@gmail.com>
2023-03-22 20:16:44 +0200

    defi page finished, bugs on tablet screen fixed

--- js/change-oriontation.js
+++ js/change-oriontation.js
@@ -1,5 +1,6 @@
 const buttons = document.querySelectorAll('.watchlist-orientation__button');
 const list = document.querySelector('.watchlist-main__list');
+const mainList = document.querySelector('.watchlist-main');
 
 document.addEventListener('click', e => {
     if (e.target.classList.contains("watchlist-orientation__button")) {
@@ -11,9 +12,11 @@ document.addEventListener('click', e => {
         if (e.target.classList.contains('watchlist-orientation__button_grid')) {
             list.classList.add("_grid");
             document.querySelector('.watchlist-legends').style.display = 'none';
+            mainList.classList.add('grid-mod');
         } else {
             list.classList.remove("_grid");
             document.querySelector('.watchlist-legends').style.display = 'grid';
+            mainList.classList.remove('grid-mod');
         }
     }
 })
